0ee9905a36ab269387aa3b49180b664115a60482
gcvin <gc.vin@qq.com>
2019-02-01 14:36:06 +0800

    fix: mod cropper style

--- src/client/views/cropper.vue
+++ src/client/views/cropper.vue
@@ -1,23 +1,40 @@
 <template>
   <div class="cropper-wrapper">
-    <div class="left-box">
-      <Upload action="" v-show="!insideSrc" :before-upload="beforeUpload">
-        <Button type="primary">上传图片</Button>
+    <div class="tip-box" v-show="cropperSrc">
+      <Upload action="" :before-upload="beforeUpload">
+        <a href="javascript:;">重新上传</a>
+        <span slot="tip">【{{ cropperType }}】当前裁切尺寸：{{ width }}x{{ height }}</span>
       </Upload>
-      <img id="cropper-image" alt="">
+    </div>
+    <div class="left-box">
+      <Upload action="" v-show="!cropperSrc" :before-upload="beforeUpload">
+        <div class="upload-box">
+          <Icon type="ios-add" />
+          <p>添加图片</p>
+        </div>
+      </Upload>
+      <img id="cropper-image">
     </div>
     <div class="right-box">
       <div id="preview-banner" class="preview-box banner">
         <a @click="initCropper('banner')" href="javascript:;"></a>
       </div>
-      <div id="preview-face" class="preview-box face">
-        <a @click="initCropper('face')" href="javascript:;"></a>
+      <div id="preview-cover" class="preview-box cover">
+        <a @click="initCropper('cover')" href="javascript:;"></a>
       </div>
-      <a href="javascript:;" v-show="bannerSrc" class="image-box banner">
-        <img :src="bannerSrc" @click="initCropper('banner')">
+      <a
+        v-if="cropperType == 'cover'"
+        href="javascript:;"
+        class="image-box banner"
+        @click="initCropper('banner')">
+        <img :src="imgSrc" v-show="imgSrc">
       </a>
-      <a href="javascript:;" v-show="faceSrc" class="image-box face">
-        <img :src="faceSrc" @click="initCropper('face')">
+      <a
+        v-else
+        href="javascript:;"
+        class="image-box cover"
+        @click="initCropper('cover')">
+        <img :src="imgSrc" v-show="imgSrc">
       </a>
     </div>
   </div>
@@ -26,14 +43,17 @@
 <script>
 import Cropper from 'cropperjs'
 import 'cropperjs/dist/cropper.min.css'
+import '@/styles/cropper.less'
 
 export default {
   data () {
     return {
       cropper: null,
-      insideSrc: '',
-      faceSrc: '',
-      bannerSrc: ''
+      cropperSrc: '',
+      cropperType: '',
+      imgSrc: '',
+      height: '',
+      width: ''
     }
   },
   methods: {
@@ -46,47 +66,52 @@ export default {
       return false
     },
     replace (src) {
+      if (!this.cropper) {
+        return false
+      }
+
       this.cropper.replace(src)
-      this.insideSrc = src
+      this.cropperSrc = src
+      this.imgSrc = ''
     },
-    initImgSrc () {
-      this.faceSrc = ''
-      this.bannerSrc = ''
-    },
-    initCropper (id) {
-      const imgUrls = {
-        banner: 'faceSrc',
-        face: 'bannerSrc'
-      }
-
-      if (this.insideSrc) {
-        const url = this.cropper.getCroppedCanvas().toDataURL()
-        this.initImgSrc()
-        this[imgUrls[id]] = url
-      }
-
-      if (this.cropper) {
-        this.cropper.destroy()
-      }
+    initCropper (type) {
+      this.cropperType = type
+      this.destroyCropper()
 
       const image = document.getElementById('cropper-image')
-      const preview = document.getElementById(`preview-${id}`)
+      const preview = document.getElementById(`preview-${type}`)
       const height = preview.clientHeight
       const width = preview.clientWidth
 
       this.cropper = new Cropper(image, {
         preview: preview,
-        checkCrossOrigin: true,
         aspectRatio: width / height
       })
 
-      this.cropper.replace(this.insideSrc)
+      if (this.cropperSrc) {
+        this.cropper.replace(this.cropperSrc)
+      }
+    },
+    destroyCropper () {
+      if (!this.cropper) {
+        return false
+      }
+
+      if (this.cropperSrc) {
+        const src = this.cropper.getCroppedCanvas().toDataURL()
+        this.imgSrc = src
+      }
+
+      this.cropper.destroy()
     }
   },
   mounted () {
-    this.$nextTick(() => {
-      this.initCropper('banner')
+    const image = document.getElementById('cropper-image')
+    image.addEventListener('crop', (event) => {
+      this.width = ~~event.detail.width
+      this.height = ~~event.detail.height
     })
+    this.initCropper('banner')
   }
 }
 </script>
@@ -95,7 +120,12 @@ export default {
   background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAAA3NCSVQICAjb4U/gAAAABlBMVEXMzMz////TjRV2AAAACXBIWXMAAArrAAAK6wGCiw1aAAAAHHRFWHRTb2Z0d2FyZQBBZG9iZSBGaXJld29ya3MgQ1M26LyyjAAAABFJREFUCJlj+M/AgBVhF/0PAH6/D/HkDxOGAAAAAElFTkSuQmCC")
 }
 
+.tip-box {
+  font-size: 14px;
+}
+
 .left-box {
+  position: relative;
   display: inline-block;
   width: 560px;
   height: 224px;
@@ -106,6 +136,19 @@ export default {
   img {
     max-width: 100%;
     display: block;
+  }
+
+  .upload-box {
+    position: absolute;
+    height: 62px;
+    width: 58px;
+    top: 80px;
+    left: 250px;
+    cursor: pointer;
+
+    i {
+      font-size: 40px;
+    }
   }
 }
 
@@ -131,7 +174,7 @@ export default {
       width: 242px !important;
     }
 
-    &.face {
+    &.cover {
       width: 170px !important;
     }
 
@@ -154,7 +197,7 @@ export default {
       top: 0;
     }
 
-    &.face {
+    &.cover {
       width: 170px;
       top: 128px;
     }
