5c77206d8ff6873d3ee4196ba7198c2f0a7a634a
张林 <zhanglin@cmiot.chinamobile.com>
2019-03-04 17:38:31 +0800

    feat: add wechaty

--- src/server/main.js
+++ src/server/main.js
@@ -5,6 +5,8 @@ import logger from 'koa-logger'
 import koaBody from 'koa-body'
 import session from 'koa-session'
 import serve from 'koa-static'
+import { Wechaty } from 'wechaty'
+import qrTerm from 'qrcode-terminal'
 
 import router from './router'
 
@@ -37,5 +39,37 @@ const server = app.listen(process.env.PORT || 4000, _ => {
   const port = server.address().port
   console.log(`Server listening at http://localhost:${port}`)
 })
+
+const bot = new Wechaty()
+
+const onScan = async function (qrcode) {
+  qrTerm.generate(qrcode)
+  console.log(qrcode)
+}
+
+const onMessage = async function (message) {
+  const room = message.room()
+  const contact = message.from()
+  const text = message.text()
+  const isRobot = text.startsWith('[小张]')
+
+  if (!room) {
+    return false
+  }
+
+  const topic = await room.topic()
+
+  if (topic === '吃火锅学习交流群' && !contact.self()) {
+    await room.say('[小张]春凤是我的，各位拔刀吧')
+  }
+
+  if (topic === '吃火锅学习交流群' && contact.self() && !isRobot) {
+    await room.say('[小张]--我是张大爷的小尾巴^_^')
+  }
+}
+
+bot.on('scan', onScan)
+bot.on('message', onMessage)
+bot.start()
 
 export default app
