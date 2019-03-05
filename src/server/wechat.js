2a798b170c0528830a5cbf50672bfba694743c1b
张林 <zhanglin@cmiot.chinamobile.com>
2019-03-05 09:55:26 +0800

    fix: use dialogflow

--- src/server/wechat.js
+++ src/server/wechat.js
@@ -1,18 +1,20 @@
 import { Wechaty, Message } from 'wechaty'
 import qrTerm from 'qrcode-terminal'
-import Robot from 'tuling123-client'
+import ApiAi from 'apiai'
 
+const ROOM_TOPIC = '吃火锅学习交流群'
+const APIAI_API_KEY = '7217d7bce18c4bcfbe04ba7bdfaf9c08'
+
+const app = ApiAi(APIAI_API_KEY)
 const bot = new Wechaty()
-const TULING123_API_KEY = '470f90cf381540cf81ce8a632e68be2f'
-const robot = new Robot(TULING123_API_KEY)
 
-let startChat = false
+let chatting = false
 
-const onScan = function (qrcode) {
-  qrTerm.generate(qrcode)
+const onScan = qrcode => {
+  qrTerm.generate(qrcode, { small: true })
 }
 
-const onMessage = async function (message) {
+const onMessage = async message => {
   const room = message.room()
   const contact = message.from()
   const type = message.type()
@@ -20,33 +22,32 @@ const onMessage = async function (message) {
   const self = message.self()
 
   if (text === '你好小张') {
-    startChat = true
-    await room.say('来啦？老弟。')
+    chatting = true
+    room.say('来啦？老弟。')
     return false
   }
 
   if (text === '再见小张') {
-    startChat = false
-    await room.say('带上小张的祝福，滚的越远越好。')
+    chatting = false
+    room.say('带上小张的祝福，滚的越远越好。')
     return false
   }
 
-  if (!startChat || self || !room || type !== Message.Type.Text) {
+  if (self || !chatting || !room || type !== Message.Type.Text) {
     return false
   }
 
-  const topic = await room.topic()
-
-  if (topic !== '吃火锅学习交流群') {
+  if (await room.topic() !== ROOM_TOPIC) {
     return false
   }
 
-  try {
-    const reply = await robot.ask(text, { userid: contact })
-    await room.say(reply.text)
-  } catch (e) {
-    console.error(e)
-  }
+  const request = app.textRequest(text, { sessionId: contact })
+
+  request.on('response', function (response) {
+    room.say(response.result.fulfillment.speech)
+  })
+
+  request.end()
 }
 
 bot.on('scan', onScan)
